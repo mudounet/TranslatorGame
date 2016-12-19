963fe312965a4144ad20f270055905967df50d36
Guillaume MANCIET <guillaume.manciet@transport.alstom.com>
2016-12-19 15:44:53 +0100

    Added expanded layout (from http://www.journaldev.com/9942/android-expandablelistview-example-tutorial).

--- translatorGame/src/main/java/com/mudounet/translatorgame/MainActivityAdapter.java
+++ translatorGame/src/main/java/com/mudounet/translatorgame/MainActivityAdapter.java
@@ -1,42 +1,98 @@
 package com.mudounet.translatorgame;
 
+import java.util.HashMap;
+import java.util.List;
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
-/**
- * Created by gmanciet on 19/12/2016.
- */
+public class MainActivityAdapter extends BaseExpandableListAdapter {
 
-public class MainActivityAdapter extends ArrayAdapter<String> {
-    private final Context context;
-    private final String[] values;
+    private Context context;
+    private List<String> expandableListTitle;
+    private HashMap<String, List<String>> expandableListDetail;
 
-    public MainActivityAdapter(Context context, String[] values) {
-        super(context, R.layout.activity_main_adapter, values);
+    public MainActivityAdapter(Context context, List<String> expandableListTitle,
+                               HashMap<String, List<String>> expandableListDetail) {
         this.context = context;
-        this.values = values;
+        this.expandableListTitle = expandableListTitle;
+        this.expandableListDetail = expandableListDetail;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.activity_main_adapter, parent, false);
+    public Object getChild(int listPosition, int expandedListPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
+                .get(expandedListPosition);
+    }
 
-        TextView firstLineView = (TextView) rowView.findViewById(R.id.firstLine);
-        TextView secondLineView = (TextView) rowView.findViewById(R.id.secondLine);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+    @Override
+    public long getChildId(int listPosition, int expandedListPosition) {
+        return expandedListPosition;
+    }
 
-        firstLineView.setText(values[position]);
-        secondLineView.setText(values[position]);
+    @Override
+    public View getChildView(int listPosition, final int expandedListPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        final String expandedListText = (String) getChild(listPosition, expandedListPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.activity_main_list_item, null);
+        }
+        TextView sectionText = (TextView) convertView
+                .findViewById(R.id.main_activity_list_item_text);
+        sectionText.setText(expandedListText);
+        return convertView;
+    }
 
-        // Change the icon :
-        //imageView.setImageResource(R.drawable.no);
-        return rowView;
+    @Override
+    public int getChildrenCount(int listPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int listPosition) {
+        return this.expandableListTitle.get(listPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this.expandableListTitle.size();
+    }
+
+    @Override
+    public long getGroupId(int listPosition) {
+        return listPosition;
+    }
+
+    @Override
+    public View getGroupView(int listPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String listTitle = (String) getGroup(listPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.activity_main_list_group, null);
+        }
+        TextView listTitleTextView = (TextView) convertView
+                .findViewById(R.id.listTitle);
+        listTitleTextView.setTypeface(null, Typeface.BOLD);
+        listTitleTextView.setText(listTitle);
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int listPosition, int expandedListPosition) {
+        return true;
     }
 }
