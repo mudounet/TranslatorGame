a6cdac7b4f5836428d3d58ddb61f872cdef80216
Guillaume MANCIET <mudounet@free.fr>
2018-01-07 23:22:07 +0100

    Adding Sync manager.

--- app/src/main/java/com/mudounet/translatoreditor/sync/Manager.java
+++ app/src/main/java/com/mudounet/translatoreditor/sync/Manager.java
@@ -1,10 +1,19 @@
 package com.mudounet.translatoreditor.sync;
 
+import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.Status;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.RepositoryCache;
 import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.util.FS;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by guillaume on 04/01/2018.
@@ -15,41 +24,89 @@ public class Manager {
     private static final org.slf4j.Logger Logger = LoggerFactory
             .getLogger(Manager.class);
 
-    private CredentialsProvider credentials;
     private Git git;
+    private boolean remoteConfigured;
+    private PersonIdent ident;
+    private String currentBranch;
 
-    public Manager(File directory, CredentialsProvider credentials) {
+    public Manager(File directory, PersonIdent ident) throws IOException, GitAPIException {
+        new Manager(directory, ident, null, null);
     }
 
-    public boolean pull() {
-        Logger.debug("Retrieving data to remote");
-        return false;
+    public Manager(File directory, PersonIdent ident, String remoteURL, CredentialsProvider credentials ) throws IOException, GitAPIException {
+
+        this.ident = ident;
+        if (RepositoryCache.FileKey.isGitRepository(directory, FS.DETECTED)) {
+
+            Logger.debug("GIT filesystem detected");
+
+            Git git = Git.open(directory);
+
+            for (Ref ref : git.getRepository().getAllRefs().values()) {
+                if (ref.getObjectId() == null)
+                    continue;
+                Logger.debug("One GIT reference found");
+                this.git = git;
+                return;
+            }
+        }
+
+        Logger.debug("Repository is not defined yet");
+        if(remoteURL != null) {
+            CloneCommand command = Git.cloneRepository().setDirectory(directory).setURI(remoteURL);
+            if (credentials != null) command.setCredentialsProvider(credentials);
+            this.git = command.call();
+            this.remoteConfigured = true;
+            Logger.debug("Initializing local repository from clone");
+        }
+        else {
+            Logger.debug("Initializing local repository");
+            this.git = Git.init().setDirectory(directory).call();
+        }
     }
 
-    public boolean push() {
-        Logger.debug("Sending data to remote");
-        return false;
+    private void _isRemoteAvailable() throws SyncException {
+        if (!this.remoteConfigured) throw new SyncException("Remote is not configured");
     }
 
     public boolean cancel() {
         Logger.debug("Reverting back changes");
-        return false;
+        throw new UnsupportedOperationException();
     }
 
     public boolean commit() {
         Logger.debug("Storing data into repository");
-        return false;
+        throw new UnsupportedOperationException();
     }
 
-    public boolean isDirty() {
+    public boolean isDirty() throws GitAPIException, IOException {
         Logger.debug("Checking is repository is dirty");
-        return false;
+        Logger.debug(git.toString());
+        Logger.debug(this.git.getRepository().resolve(Constants.HEAD).getName());
+        Status status = git.status().call();
+        Logger.debug(status.toString());
+        return status.hasUncommittedChanges();
     }
 
     public File getFiles() {
         Logger.debug("Returning all files");
-        return null;
+        throw new UnsupportedOperationException();
     }
 
+    public boolean pull() throws SyncException {
+        Logger.debug("Retrieving data to remote");
+        _isRemoteAvailable();
+        throw new UnsupportedOperationException();
+    }
 
+    public boolean push() throws SyncException {
+        Logger.debug("Sending data to remote");
+        _isRemoteAvailable();
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean syncNeeded() throws SyncException {
+        _isRemoteAvailable();
+        throw new UnsupportedOperationException();
+    }
 }
