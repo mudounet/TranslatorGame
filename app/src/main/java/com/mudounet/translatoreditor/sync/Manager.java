a1a4872faede3270daafe5a82250984399e806ef
Guillaume MANCIET <mudounet@free.fr>
2018-01-08 23:05:58 +0100

    Correction of remote configuration.

--- app/src/main/java/com/mudounet/translatoreditor/sync/Manager.java
+++ app/src/main/java/com/mudounet/translatoreditor/sync/Manager.java
@@ -1,6 +1,7 @@
 package com.mudounet.translatoreditor.sync;
 
 import org.eclipse.jgit.api.CloneCommand;
+import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
@@ -14,6 +15,8 @@ import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.merge.MergeStrategy;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.URIish;
 import org.eclipse.jgit.util.FS;
 import org.slf4j.LoggerFactory;
@@ -22,6 +25,8 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by guillaume on 04/01/2018.
@@ -61,7 +66,7 @@ public class Manager {
         }
 
         if(remoteURL != null) {
-            _setRemote(remoteURL, credentials);
+            _setRemote(remoteURL, "master", credentials);
             this.git.pull();
             Logger.debug("Initializing local repository from remote+pull");
         }
@@ -71,13 +76,26 @@ public class Manager {
         return !this.git.getRepository().getRemoteNames().isEmpty();
     }
 
-    private void _setRemote(String remoteURL, CredentialsProvider credentials) throws IOException, URISyntaxException {
+    private void _setRemote(String remoteURL, String branch, CredentialsProvider credentials) throws IOException, URISyntaxException, GitAPIException {
         if(credentials != null) this.credentials = credentials;
 
-
         StoredConfig config = git.getRepository().getConfig();
-        config.setString("remote", "origin", "url", new URIish(remoteURL).toString());
+
+        RemoteConfig remoteConfig = new RemoteConfig(config, "origin");
+        remoteConfig.addURI(new URIish(remoteURL));
+        remoteConfig.addFetchRefSpec(new RefSpec("+refs/heads/" + branch + ":refs/remotes/origin/" + branch));
+
+        remoteConfig.update(config);
         config.save();
+
+        git.fetch().call();
+        git.branchCreate().setName(branch).setStartPoint("origin/" +  branch)
+                .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK).setForce(true).call();
+        git.checkout().setName( branch).call();
+
+        git.fetch().call();
+        git.reset().setRef("origin/" + branch).call();
+        this.currentBranch = branch;
     }
 
     public boolean cancel() {
