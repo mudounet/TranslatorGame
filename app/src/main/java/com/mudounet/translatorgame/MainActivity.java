8b55fbe44f199031d469f79fad648ae66db5a548
Guillaume MANCIET <mudounet@free.fr>
2018-01-16 23:59:39 +0100

    Changed displayed items.

--- app/src/main/java/com/mudounet/translatorgame/MainActivity.java
+++ app/src/main/java/com/mudounet/translatorgame/MainActivity.java
@@ -64,12 +64,8 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void ajouterVilles() {
-        cities.add(new MyObject("France", "test", "http://www.telegraph.co.uk/travel/destination/article130148.ece/ALTERNATES/w620/parisguidetower.jpg"));
-        cities.add(new MyObject("Angleterre", "test", "http://www.traditours.com/images/Photos%20Angleterre/ForumLondonBridge.jpg"));
-        cities.add(new MyObject("Allemagne", "test", "http://tanned-allemagne.com/wp-content/uploads/2012/10/pano_rathaus_1280.jpg"));
-        cities.add(new MyObject("Espagne","test", "http://www.sejour-linguistique-lec.fr/wp-content/uploads/espagne-02.jpg"));
-        cities.add(new MyObject("Italie","", "http://retouralinnocence.com/wp-content/uploads/2013/05/Hotel-en-Italie-pour-les-Vacances2.jpg"));
-        cities.add(new MyObject("Russie","test", "http://www.choisir-ma-destination.com/uploads/_large_russie-moscou2.jpg"));
+        cities.add(new MyObject("Lessons", "lessons.xml", ""));
+        cities.add(new MyObject("Declensions", "Declensions.xml", ""));
     }
 
     @Override
